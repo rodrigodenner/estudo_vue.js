278a8f2abd994bd539cf5e39372f32f10440ad39
souza-su <rodrigodenner.art@gmail.com>
2023-10-21 19:49:57 -0300

    estudo v-bind

--- src/App.vue
+++ src/App.vue
@@ -21,11 +21,5 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,12 +1,18 @@
 <template>
   <div>
-    <h2>Lista de Tarefas</h2>
-    <img src="https://placehold.co/400" alt="Foto aleatoria">
-    <ul>
-      <li v-for="todo in todos" v-bind:key=todo.id>
-        {{ todo.title }}
-      </li>
-    </ul>
+    <h2>Lista de Usuários</h2>
+
+    <div v-for="todo in todos" v-bind:key=todo.id>
+        <div v-if="todo.imgSrc">
+            Olá {{ todo.fullName}}
+            <img v-bind:src="todo.imgSrc" :alt="todo.imgAlt" >
+            <hr>
+        </div> 
+        <div v-else>
+        Olá {{ todo.fullName }} <br><br> Que pena voce está sem foto <br>
+        <a href="">Adicionar foto</a>
+        </div>
+    </div>
   </div>
 </template>
 
@@ -14,33 +20,25 @@
 export default {
   data() {
     return {
+
       todos: [
         {
           "userId": 1,
           "id": 1,
-          "title": "delectus aut autem",
-          "completed": false
+          "fullName": "Rodrigo Denner",
+          "imgSrc":'https://static.vecteezy.com/ti/vetor-gratis/t2/4607791-cara-de-homem-emotiva-icone-sorridente-personagem-masculino-de-camisa-azul-ilustracao-isolado-no-branco-feliz-humano-psicologico-retrato-emocoes-positivas-usuario-avatar-para-app-web-design-vetor.jpg',
+          "imgAlt":"Foto aleatoria"
         },
         {
           "userId": 1,
-          "id": 2,
-          "title": "quis ut nam facilis et officia qui",
-          "completed": false
+          "id": 1,
+          "fullName": "Francisco Denner",
+          "imgSrc":"https://static.vecteezy.com/ti/vetor-gratis/t2/2002403-homem-com-barba-avatar-personagem-icone-isolado-gratis-vetor.jpg",
+          "imgAlt":"Foto aleatoria"
         },
-        {
-          "userId": 1,
-          "id": 3,
-          "title": "fugiat veniam minus",
-          "completed": false
-        },
-        {
-          "userId": 1,
-          "id": 4,
-          "title": "et porro tempora",
-          "completed": true
-        }
-      ]
-    }
+        
+      ],
+    };
   }
-}
+};
 </script>
